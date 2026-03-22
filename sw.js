b873c181afc026bce5c56f018d1c0ddeb70f1106
mmt8129 <06mmt8129@gmail.com>
2026-03-22 08:44:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,3 +20,14 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});

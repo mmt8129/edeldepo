45db936fe484fde2ee34abab0ac9da8f402ffa62
mmt8129 <06mmt8129@gmail.com>
2026-03-22 08:50:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,37 @@
-const CACHE_NAME = 'depo-cache-v1';
-const REPO_PATH = '/edeldepo';
-
+const CACHE_NAME = 'depo-raf-v1';
 const urlsToCache = [
-  REPO_PATH + '/',
-  REPO_PATH + '/index.html',
-  REPO_PATH + '/manifest.json',
+  '/edeldepo/',
+  '/edeldepo/index.html',
+  '/edeldepo/veri.html',
+  '/edeldepo/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache açıldı');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.log('Cache hatası:', err))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch(() => new Response('Ağ hatası', { status: 404 }))
   );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
-  );
+  event.waitUntil(clients.claim());
+  console.log('Service Worker aktif');
 });
